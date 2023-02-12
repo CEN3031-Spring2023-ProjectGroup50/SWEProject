af8b33a06598c9fd7955c2743d2fa1e52f08da1d
Wes <pimpbot@ufl.edu>
2023-02-12 05:07:38 -0600

    change picture locations to assets

--- recipeApp/client/src/app/recipe-dashboard/recipe-dashboard.component.ts
+++ recipeApp/client/src/app/recipe-dashboard/recipe-dashboard.component.ts
@@ -14,8 +14,8 @@ export class RecipeDashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Takitos', author: "Jane Doe's recipe", cols: 1, rows: 1, description: "This recipe is so deliscious WOW! And this description is really super detailed and tells you everything you would ever need to know at-a-glance. Easy to make, easy to clean, healthy.", headerimagecss: "header-image-mallow", mainimage: "../foodimage.jpg"},
-          { title: 'Pancakes', author: "Mallow's recipe", cols: 1, rows: 1, description: "This is a test description", headerimagecss: "header-image-mallow", mainimage: "../imgpancakes.jpeg"},
+          { title: 'Takitos', author: "Jane Doe's recipe", cols: 1, rows: 1, description: "This recipe is so deliscious WOW! And this description is really super detailed and tells you everything you would ever need to know at-a-glance. Easy to make, easy to clean, healthy.", headerimagecss: "header-image-mallow", mainimage: "/assets/images/foodimage.jpg"},
+          { title: 'Pancakes', author: "Mallow's recipe", cols: 1, rows: 1, description: "This is a test description", headerimagecss: "header-image-mallow", mainimage: "/assets/images/imgpancakes.jpeg"},
           { title: 'Card 3A', author: "PERSON3 recipe", cols: 1, rows: 1, description: "content 3A", headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg"},
           { title: 'Card 4A', author: "PERSON4 recipe", cols: 1, rows: 1, description: "content 4A", headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg" },
           { title: 'Card 5A', author: "PERSON4 recipe", cols: 1, rows: 1, description: "content 5A", headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg" },
@@ -24,8 +24,8 @@ export class RecipeDashboardComponent {
       }
 
       return [
-        { title: 'Takitos', author: "Jane Doe's recipe", cols: 1, rows: 1, description: "This recipe is so deliscious WOW! And this description is really super detailed and tells you everything you would ever need to know at-a-glance. Easy to make, easy to clean, healthy.", headerimagecss: "header-image-mallow", mainimage: "../foodimage.jpg" },
-        { title: 'Pancakes', author: "Mallow's recipe", cols: 1, rows: 1, description: "This is a test description", headerimagecss: "header-image-mallow", mainimage: "../imgpancakes.jpeg" },
+        { title: 'Takitos', author: "Jane Doe's recipe", cols: 1, rows: 1, description: "This recipe is so deliscious WOW! And this description is really super detailed and tells you everything you would ever need to know at-a-glance. Easy to make, easy to clean, healthy.", headerimagecss: "header-image-mallow", mainimage: "/assets/images/foodimage.jpg" },
+        { title: 'Pancakes', author: "Mallow's recipe", cols: 1, rows: 1, description: "This is a test description", headerimagecss: "header-image-mallow", mainimage: "/assets/images/imgpancakes.jpeg" },
         { title: 'Card 3B', author: "PERSON2B recipe", cols: 1, rows: 1, description: "content 3B",headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg" },
         { title: 'Card 4B', author: "PERSON2B recipe", cols: 1, rows: 1, description: "content 4B", headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg" },
         { title: 'Card 5A', author: "PERSON4 recipe", cols: 1, rows: 1, description: "content 5A", headerimagecss: "header-image-mallow", mainimage: "https://material.angular.io/assets/img/examples/shiba2.jpg" },
