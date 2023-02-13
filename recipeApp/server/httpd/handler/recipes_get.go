e1748853cd0f569d37a28c8a9053263d0c3acbe5
Wes <pimpbot@ufl.edu>
2023-02-12 21:36:10 -0600

    added keywords search

--- recipeApp/server/httpd/handler/recipes_get.go
+++ recipeApp/server/httpd/handler/recipes_get.go
@@ -17,7 +17,30 @@ func RecipeGet() gin.HandlerFunc {
 			fmt.Printf("key = %v, value(s) = %v\n", key, values)
 		}
 
-		if len(paramPairs["id"]) > 0 {
+		if len(paramPairs["keyword"]) > 0 {
+			var recipe []models.Recipe
+			var wildcardIngredients []string
+			var wildcardInstructions []string
+			var wildcardTitle []string
+			for index := range paramPairs["keyword"] {
+				wildcardIngredients = append(wildcardIngredients, "ingredients ILIKE '%"+paramPairs["keyword"][index]+"%'")
+				wildcardInstructions = append(wildcardInstructions, "ingredients ILIKE '%"+paramPairs["keyword"][index]+"%'")
+				wildcardTitle = append(wildcardTitle, "ingredients ILIKE '%"+paramPairs["keyword"][index]+"%'")
+			}
+
+			initialize.Db.Table("recipe").Where(strings.Join(wildcardIngredients, " AND ")).
+				Or(strings.Join(wildcardInstructions, " AND ")).Or(strings.Join(wildcardTitle, " AND ")).Find(&recipe)
+
+			if len(recipe) == 0 {
+				c.JSON(http.StatusBadRequest, gin.H{
+					//TODO - FIX FOR MULTIPLE STRINGS
+					"error": "No recipes found containing the term " + paramPairs["keyword"][0],
+				})
+				return
+			}
+
+			c.JSON(http.StatusOK, recipe)
+		} else if len(paramPairs["id"]) > 0 {
 			var recipe models.Recipe
 			initialize.Db.Table("recipe").Where("rid = ?", paramPairs["id"]).First(&recipe)
 
@@ -35,7 +58,7 @@ func RecipeGet() gin.HandlerFunc {
 
 			var recipe []models.Recipe
 			initialize.Db.Table("recipe").
-				Where("ingredients LIKE ?", ingredientText).Find(&recipe)
+				Where("ingredients ILIKE ?", ingredientText).Find(&recipe)
 
 			// return error if recipe not found
 			if len(recipe) == 0 {
@@ -51,7 +74,7 @@ func RecipeGet() gin.HandlerFunc {
 			fmt.Printf("this is of type %T", paramPairs["ingredient"])
 			var wildcardIngredients []string
 			for index := range paramPairs["ingredient"] {
-				wildcardIngredients = append(wildcardIngredients, "ingredients LIKE '%"+paramPairs["ingredient"][index]+"%'")
+				wildcardIngredients = append(wildcardIngredients, "ingredients ILIKE '%"+paramPairs["ingredient"][index]+"%'")
 			}
 			var recipe []models.Recipe
 			initialize.Db.Table("recipe").Where(strings.Join(wildcardIngredients, " AND ")).Find(&recipe)
