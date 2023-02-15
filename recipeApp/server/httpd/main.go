d3f6bf99e8bc693cf88873dd33247a3ffb810f3c
Wes <pimpbot@ufl.edu>
2023-02-14 20:39:30 -0600

    removed vestigial backend pieces

--- recipeApp/server/httpd/main.go
+++ recipeApp/server/httpd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"recipeApp/controllers"
 	"recipeApp/httpd/handler"
-	"recipeApp/httpd/platform/newsfeed"
 	"recipeApp/initialize"
 
 	"github.com/gin-gonic/gin"
@@ -15,18 +14,14 @@ func init() {
 }
 
 func main() {
-	feed := newsfeed.New()
 
 	r := gin.Default()
 
 	server := r.Group("/server")
 	{
-		server.GET("/ping", handler.PingGet())
-		server.GET("/newsfeed", handler.NewsfeedGet(feed))
-		server.GET("/recipes", handler.RecipeGet())
-		server.POST("/newsfeed", handler.NewsfeedPost(feed))
 		server.POST("/register", controllers.Register)
 		server.POST("/login", controllers.Login)
+		server.GET("/recipes", handler.RecipeGet())
 		server.POST("/recipes/add", handler.CreateRecipe())
 		server.GET("/recipes/bypage", handler.RecipeGetByPage())
 		server.DELETE("/recipes/delete/:id", handler.DeleteRecipe())
