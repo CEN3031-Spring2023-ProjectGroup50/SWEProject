7421c5516ee3fdb1d7f29ae725ca12c2d4575155
Wes <pimpbot@ufl.edu>
2023-02-25 13:54:30 -0600

    get recipes by page covered in testing

--- recipeApp/server/main_test.go
+++ recipeApp/server/main_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"recipeApp/httpd/handler"
 	"recipeApp/models"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -123,5 +125,56 @@ func TestRecipeGetByIngredient(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code, "Unable to retrieve recipes with ingredient 'cornichon'.")
 	assert.NotEmpty(t, recipes)
 	assert.Equal(t, hasKeyword, true, "One or more recipes did not contain ingredient 'cornichon'.")
+
+}
+
+func TestRecipeGetByPage(t *testing.T) {
+
+	r := SetUpRouter()
+
+	r.GET("/server/recipes/bypage", handler.RecipeGetByPage())
+
+	type test struct {
+		input string
+		page  int
+		size  int
+	}
+	//var recipes []models.Recipe
+	url := "/server/recipes/bypage"
+	var recipes []models.Recipe
+
+	testConds := []test{
+		{input: url + "?page=1&per_page=5", page: 1, size: 5},
+		{input: url + "?page=2&per_page=5", page: 2, size: 5},
+		{input: url + "?page=10&per_page=5", page: 10, size: 5},
+		{input: url + "?page=100&per_page=5", page: 100, size: 5},
+		{input: url + "?page=1&per_page=10", page: 1, size: 10},
+		{input: url + "?page=2&per_page=10", page: 2, size: 10},
+		{input: url + "?page=10&per_page=10", page: 10, size: 10},
+		{input: url + "?page=100&per_page=10", page: 100, size: 10},
+		{input: url + "?page=1&per_page=25", page: 1, size: 25},
+		{input: url + "?page=2&per_page=25", page: 2, size: 25},
+		{input: url + "?page=10&per_page=25", page: 10, size: 25},
+		{input: url + "?page=100&per_page=25", page: 100, size: 25},
+		{input: url + "?page=1&per_page=100", page: 1, size: 100},
+		{input: url + "?page=2&per_page=100", page: 2, size: 100},
+		{input: url + "?page=10&per_page=100", page: 10, size: 100},
+		{input: url + "?page=100&per_page=100", page: 100, size: 100},
+	}
+
+	for _, tc := range testConds {
+		req, _ := http.NewRequest("GET", tc.input, nil)
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		fmt.Print(w)
+
+		json.Unmarshal(w.Body.Bytes(), &recipes)
+		lastrID := int(recipes[len(recipes)-1].Rid)
+
+		assert.Equal(t, http.StatusOK, w.Code, "Unable to retrieve recipes with test conditions "+tc.input)
+		assert.Equal(t, tc.size, len(recipes), "Did not retrieve the correct number of recipes, expected 10.")
+		assert.Equal(t, tc.page*tc.size-1, int(lastrID), "Last retrieved element was different, expected "+strconv.Itoa(lastrID))
+
+	}
 
 }
