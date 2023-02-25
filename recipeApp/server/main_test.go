bcf4b757c1ea34afdc4bc01190b34b805673db6f
Wes <pimpbot@ufl.edu>
2023-02-25 09:35:41 -0600

    Added more assertions to TestRecipesRoute. Added TestRecipeGetByID

--- recipeApp/server/main_test.go
+++ recipeApp/server/main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"recipeApp/httpd/handler"
+	"recipeApp/models"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -25,5 +27,31 @@ func TestRecipesRoute(t *testing.T) {
 
 	r.ServeHTTP(w, req)
 
+	var recipes []models.Recipe
+	json.Unmarshal(w.Body.Bytes(), &recipes)
+
 	assert.Equal(t, http.StatusOK, w.Code, "Did not retrieve all recipes.")
+	assert.NotEmpty(t, recipes, "Empty result for all recipes.")
+	assert.Greater(t, len(recipes), 13000, "Not enough recipes returned.")
+
+}
+
+func TestRecipeGetByID(t *testing.T) {
+
+	r := SetUpRouter()
+
+	r.GET("/server/recipes", handler.RecipeGet())
+
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "/server/recipes?id=7", nil)
+
+	r.ServeHTTP(w, req)
+
+	var recipe models.Recipe
+	json.Unmarshal(w.Body.Bytes(), &recipe)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Unable to retrieve recipe by ID.")
+	assert.Equal(t, recipe.Rid, uint(7), "Retrieved the wrong recipe.")
+
 }
