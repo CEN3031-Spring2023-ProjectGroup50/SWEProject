39e2eb1629c0ba7a26ebb93c8129ae9dbd505cce
Emily Anderson <em.anderson@ufl.edu>
2023-02-27 16:48:23 -0500

    Welcome Component Beginning Unit Tests

--- recipeApp/client/src/app/welcome.component.spec.ts
+++ recipeApp/client/src/app/welcome.component.spec.ts
@@ -1,11 +1,51 @@
+import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing';
 import { WelcomeComponent } from './welcome.component';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { AppModule } from './app.module';
+import {By} from '@angular/platform-browser';
+
 
 describe('WelcomeComponent', () => {
 
-    it('should be fine (this is to confirm the test is set up properly)', () => {
-        expect(true).toBeTruthy();
-    })
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
 
-    // INSERT UNIT TESTS AND DESCRIPTIONS BELOW
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ WelcomeComponent ],
+        imports:[
+          HttpClientModule,
+          AppModule
+        ],
+        providers: [
+          HttpClient
+        ]
+      }).compileComponents();
+   }));
+
+   beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  })
+
+
+  //should display Welcome Banner along the top saying "Welcome to Mallow"
+  it('should display "Welcome to Mallow" in the initial span', () => {
+    expect(component.title).toBe('Welcome to Mallow');
+    const title = fixture.debugElement.query(By.css('#welcome')).nativeElement;
+    expect(title.innerHTML).toBe('Welcome to Mallow');
+  });
+
+  //should display menu icon, favorite icon, and share icons in the same banner
+
+  //should display the main descriptions "Discover Recipes", "Set up your meal plan for the week",
+  //and "Generate your grocery list"
+
 
 })
