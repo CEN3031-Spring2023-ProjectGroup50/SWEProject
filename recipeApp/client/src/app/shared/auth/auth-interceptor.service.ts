913108e473af2fd5820061b23c6e10084a2fd1d8
Seth Paul <seth.paul@ufl.edu>
2023-03-12 20:17:12 -0400

    refresh token overhaul

--- recipeApp/client/src/app/shared/auth/auth-interceptor.service.ts
+++ recipeApp/client/src/app/shared/auth/auth-interceptor.service.ts
@@ -1,31 +1,62 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor, HttpHeaders, HttpErrorResponse, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { Observable, tap, catchError, switchMap, throwError } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
+    private isRefreshing = false;
 
-    constructor(private injector: Injector) { }
+    constructor(private authService: AuthService) { }
 
-    intercept(req:any, next:any) {
-        const authService = this.injector.get(AuthService);
-        const authRequest = req.clone({
-            // tslint:disable-next-line:max-line-length
-            headers: req.headers.set('Authorization', 'Bearer ' + authService.token)
-            .set('Refresh', 'Bearer ' + authService.refreshToken)
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const headers = new HttpHeaders({
+          'Authorization': 'Bearer ' + this.authService.token,
+          'Refresh': 'Bearer ' + this.authService.refreshToken
         });
 
-        return next.handle(authRequest).pipe( tap(() => {},
-        (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          if (err.status !== 401) {
-           return;
-          }
-          //authService.refresh();
-          authService.logout();
+        req = req.clone({headers});
+
+        return next.handle(req).pipe(
+          catchError((error) => {
+            if (
+              error instanceof HttpErrorResponse &&
+              req.url.includes('server/account') &&
+              error.status === 401
+            ) {
+              console.log("error caught");
+              return this.handle401Error(req, next);
+            }
+    
+            return throwError(() => error);
+          })
+        );
+    }
+
+    private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+      if (!this.isRefreshing) {
+        this.isRefreshing = true;
+  
+        if (this.authService.isAuthenticated && this.authService.refreshToken) {
+          return this.authService.refresh().pipe(
+            switchMap(() => {
+              this.isRefreshing = false;
+  
+              return next.handle(request);
+            }),
+            catchError((error) => {
+              this.isRefreshing = false;
+  
+              if (error instanceof HttpErrorResponse && error.status === 401) {
+                this.authService.logout();
+              }
+  
+              return throwError(() => error);
+            })
+          );
         }
-      }));
+      }
+  
+      return next.handle(request);
     }
 }
