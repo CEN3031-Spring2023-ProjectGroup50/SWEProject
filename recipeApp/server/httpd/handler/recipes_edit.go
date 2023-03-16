e3c3bfbea9582307e923c8e064734b46838a6e7f
Seth Paul <seth.paul@ufl.edu>
2023-03-16 02:01:13 -0400

    add error if record to delete/edit not found

--- recipeApp/server/httpd/handler/recipes_edit.go
+++ recipeApp/server/httpd/handler/recipes_edit.go
@@ -41,7 +41,7 @@ func EditRecipe() gin.HandlerFunc {
 
 		var recipe models.Recipe
 		initialize.Db.Table("recipe_1").Where("rid = ?", id).Find(&recipe)
-		if recipe.Uid == mallowid {
+		if recipe.Uid == mallowid || recipe.Uid == 0 {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": "Could not edit recipe",
 			})
