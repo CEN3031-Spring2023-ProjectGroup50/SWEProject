de0bdea9ac60b1b4ac03b81ed34f1b6b7282856f
Shannon <shanhix1@Outlook.com>
2023-03-18 16:29:04 -0700

    Populate recipe details dialog with real recipe details from recipes component API call

--- recipeApp/client/src/app/recipe-details/recipe-details.component.ts
+++ recipeApp/client/src/app/recipe-details/recipe-details.component.ts
@@ -1,25 +1,43 @@
-import {Component} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import {Component, Input, Inject} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+interface IRecipeItem {
+  Rid: number,
+  Title: string,
+  Ingredients: string,
+  Instructions: string,
+  Image_name: string,
+  Uid: number,
+  Image: Uint8Array[],
+}
 
 @Component({
   selector: 'recipe-details',
   templateUrl: 'recipe-details.component.html',
 })
+
 export class RecipeDetailsModule {
+
+  @Input() recipe: IRecipeItem;
+
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
-    const dialogRef = this.dialog.open(RecipeDetailsContentModule);
+    const dialogRef = this.dialog.open(RecipeDetailsContentModule, {
+      data: {
+        rID: this.recipe.Rid,
+        Title: this.recipe.Title,
+        Ingredients: formatIngredients(this.recipe.Ingredients),
+        Instructions: formatInstructions(this.recipe.Instructions),
+        Image_name: this.recipe.Image_name,
+        Uid: this.recipe.Uid,
+        Image: this.recipe.Image,
+      }
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
-
-    // If you want to share data with your dialog, you can use the data option to pass information to the dialog component.
-    // let dialogRef = dialog.open(YourDialog, {
-    //     data: { name: 'austin' },
-    //   });
   }
 }
 
@@ -29,4 +47,18 @@ export class RecipeDetailsModule {
   styleUrls: ['./recipe-details.component.css']
 
 })
-export class RecipeDetailsContentModule {}
+export class RecipeDetailsContentModule {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public recipe: any) {}
+
+}
+
+function formatIngredients(Ingredients: string,) {
+  let result = Ingredients.substring(2, Ingredients.length-2);
+  result = result.replaceAll("\', \'" , "\n");
+  return result;
+}
+
+function formatInstructions(Instructions: string,) {
+  return Instructions.replaceAll("\n", "\n\n");
+}
