3f2659cd55239e047ee13c00adace9b5efc8680f
Seth Paul <seth.paul@ufl.edu>
2023-03-23 21:07:14 -0400

    http requests now wait for token refresh

--- recipeApp/client/src/app/shared/auth/auth-interceptor.service.ts
+++ recipeApp/client/src/app/shared/auth/auth-interceptor.service.ts
@@ -1,67 +1,97 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor, HttpHeaders, HttpErrorResponse, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { Observable, tap, catchError, switchMap, throwError } from 'rxjs';
+import { Observable, tap, catchError, switchMap, throwError, Subject } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
+    authService = this.injector.get(AuthService);  
     private isRefreshing = false;
 
-    constructor(private authService: AuthService) { }
+    tokenRefreshedSource = new Subject<void>();
+    tokenRefreshed$ = this.tokenRefreshedSource.asObservable();
+
+    constructor(private injector: Injector) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const headers = new HttpHeaders({
-          'Authorization': 'Bearer ' + this.authService.token,
-          'Refresh': 'Bearer ' + this.authService.refreshToken
-        });
-
-        req = req.clone({headers});
+        req = this.addHeader(req);
 
         return next.handle(req).pipe(
           catchError((error) => {
-            if (
-              error instanceof HttpErrorResponse &&
-              req.url.includes('server/account') &&
-              error.status === 401
-            ) {
-              return this.handleAuthError(req, next);
+            return this.handleError(error, req, next);
+          })
+        ) as Observable<HttpEvent<any>>;
+    }
+
+    private handleError(error: any, request?: HttpRequest<any>, next?: HttpHandler): any {
+      if (error.status === 401 && request && next) {
+        if (request.url.includes("refresh")) {
+          return throwError(() => error);
+        }
+
+        return this.refreshToken().pipe(
+          switchMap(() => {
+            //change headers to new tokens
+            request = this.addHeader(request!);
+            return next.handle(request);
+          }),
+          catchError((error) => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+              this.authService.logout();
             }
-    
+            else {
+              return this.handleError(error);
+            }
+
             return throwError(() => error);
           })
         );
+      }
+      else if (error.status === 403) {
+        this.authService.logout();
+      }
+
+      return throwError(() => error);
     }
 
-    private handleAuthError(request: HttpRequest<any>, next: HttpHandler) {
-      if (!this.isRefreshing) {
-        this.isRefreshing = true;
-  
-        if (this.authService.isAuthenticated && this.authService.refreshToken) {
-          return this.authService.refresh().pipe(
-            switchMap(() => {
-              this.isRefreshing = false;
+    private addHeader(request: HttpRequest<any>) {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + this.authService.token,
+        'Refresh': 'Bearer ' + this.authService.refreshToken
+      });
+      console.log(this.authService.token);
+      console.log(this.authService.refreshToken);
 
-              //change headers to new tokens
-              const headers = new HttpHeaders({
-                'Authorization': 'Bearer ' + this.authService.token,
-                'Refresh': 'Bearer ' + this.authService.refreshToken
-              });
-              request = request.clone({headers});
-              return next.handle(request);
-            }),
-            catchError((error) => {
-              this.isRefreshing = false;
-  
-              if (error instanceof HttpErrorResponse && error.status === 401) {
-                this.authService.logout();
-              }
-  
-              return throwError(() => error);
-            })
-          );
-        }
+      if (headers) {
+        return request.clone({ headers });
       }
-  
-      return next.handle(request);
+
+      return request;
+    }
+
+    private refreshToken(): Observable<any> {
+      if (this.isRefreshing) {
+        return new Observable(observer => {
+          this.tokenRefreshed$.subscribe(() => {
+            observer.next();
+            observer.complete();
+          });
+        });
+      }
+      else {
+        this.isRefreshing = true;
+
+        return this.authService.refresh().pipe(
+          tap(() => {
+            this.isRefreshing = false;
+            this.tokenRefreshedSource.next();
+          }),
+          catchError((error) => {
+            this.isRefreshing = false;
+            this.authService.logout();
+            return throwError(() => error);
+          })
+        );
+      }
     }
 }
