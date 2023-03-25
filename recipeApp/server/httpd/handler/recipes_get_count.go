97b54f022425e80c7245e491688ecf5c2f240b4e
Wes <pimpbot@ufl.edu>
2023-03-25 09:33:17 -0500

    moving wildcards out of conditionals in get count

--- recipeApp/server/httpd/handler/recipes_get_count.go
+++ recipeApp/server/httpd/handler/recipes_get_count.go
@@ -17,15 +17,21 @@ func RecipeGetCount() gin.HandlerFunc {
 		paramPairs := c.Request.URL.Query()
 		var total int64
 
+		var wildcardIngredients []string
+		var wildcardInstructions []string
+		var wildcardTitle []string
+		for index := range paramPairs["keyword"] {
+			wildcardIngredients = append(wildcardIngredients, "ingredients ILIKE '%"+paramPairs["keyword"][index]+"%'")
+			wildcardInstructions = append(wildcardInstructions, "instructions ILIKE '%"+paramPairs["keyword"][index]+"%'")
+			wildcardTitle = append(wildcardTitle, "title ILIKE '%"+paramPairs["keyword"][index]+"%'")
+		}
+		var wildcardSoloIngredients []string
+		for index := range paramPairs["ingredient"] {
+			wildcardSoloIngredients = append(wildcardSoloIngredients, "ingredients ILIKE '%"+paramPairs["ingredient"][index]+"%'")
+		}
+
 		if len(paramPairs["keyword"]) > 0 {
-			var wildcardIngredients []string
-			var wildcardInstructions []string
-			var wildcardTitle []string
-			for index := range paramPairs["keyword"] {
-				wildcardIngredients = append(wildcardIngredients, "ingredients ILIKE '%"+paramPairs["keyword"][index]+"%'")
-				wildcardInstructions = append(wildcardInstructions, "instructions ILIKE '%"+paramPairs["keyword"][index]+"%'")
-				wildcardTitle = append(wildcardTitle, "title ILIKE '%"+paramPairs["keyword"][index]+"%'")
-			}
+
 			if uid == 0 {
 
 				initialize.Db.Table("users").Select("users.email", "recipe.*").Joins("join recipe on recipe.uid = users.id").Where(strings.Join(wildcardIngredients, " AND ")).
@@ -41,17 +47,14 @@ func RecipeGetCount() gin.HandlerFunc {
 
 			}
 		} else if len(paramPairs["ingredient"]) > 0 {
-			var wildcardIngredients []string
-			for index := range paramPairs["ingredient"] {
-				wildcardIngredients = append(wildcardIngredients, "ingredients ILIKE '%"+paramPairs["ingredient"][index]+"%'")
-			}
+
 			if uid == 0 {
 
-				initialize.Db.Table("recipe").Where(strings.Join(wildcardIngredients, " AND ")).
+				initialize.Db.Table("recipe").Where(strings.Join(wildcardSoloIngredients, " AND ")).
 					Order("rid").Count(&total)
 			} else {
 				subq := initialize.Db.Table("recipe").Where("uid =?", uid)
-				initialize.Db.Table("(?) as u", subq).Where(strings.Join(wildcardIngredients, " AND ")).
+				initialize.Db.Table("(?) as u", subq).Where(strings.Join(wildcardSoloIngredients, " AND ")).
 					Order("rid").Count(&total)
 			}
 
