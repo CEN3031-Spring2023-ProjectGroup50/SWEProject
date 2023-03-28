2aabbc3083087e081c7235a6486bb2ef21b77a78
Shannon <shanhix1@Outlook.com>
2023-03-27 21:59:30 -0700

    Added basic tests for registerForm; one is not passing, just like login editor scenario

--- recipeApp/client/src/app/registerform/registerform.component.spec.ts
+++ recipeApp/client/src/app/registerform/registerform.component.spec.ts
@@ -32,7 +32,7 @@ import { CommonModule } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '../shared/auth/auth.service';
 
-describe('registrationForm', () => {
+describe('registerForm', () => {
 
   let component: registerFormComponent;
   let fixture: ComponentFixture<registerFormComponent>;
@@ -92,21 +92,14 @@ describe('registrationForm', () => {
     }).compileComponents();
  }));
 
-  it('should compile', waitForAsync(() => {
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create the registerform component', waitForAsync(() => {
     const fixture = TestBed.createComponent(registerFormComponent);
     const regForm = fixture.debugElement.componentInstance;
     expect(regForm).toBeTruthy();
   }));
-
-
-  // The email field, password field, and register button should render
-
-  // Display error message when email is blank
-
-  // Display error message when password is blank
-
-  // Display error message when email & password combo exists
-
-  // Successful login should route the user to home page
 
 })
