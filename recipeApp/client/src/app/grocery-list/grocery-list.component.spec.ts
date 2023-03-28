3e4536aae99dce285c33b94b5c4cf41ecefcff7d
Wes <pimpbot@ufl.edu>
2023-03-27 22:53:44 -0500

    grocery list and list-page tests

---
+++ recipeApp/client/src/app/grocery-list/grocery-list.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing';
+import { GroceryListComponent } from './grocery-list.component';
+import { AppModule } from '../app.module';
+import { WelcomeComponent } from '../welcome/welcome.component';
+
+describe('GroceryListComponent', () => {
+  let component: GroceryListComponent;
+  let fixture: ComponentFixture<GroceryListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [GroceryListComponent],
+      imports: [
+        AppModule
+      ],
+      
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GroceryListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  })
+
+  it('should create the welcome component', waitForAsync(() => {
+    const fixture = TestBed.createComponent(WelcomeComponent);
+    const appHeader = fixture.debugElement.componentInstance;
+    expect(appHeader).toBeTruthy();}))
+
+
+});
