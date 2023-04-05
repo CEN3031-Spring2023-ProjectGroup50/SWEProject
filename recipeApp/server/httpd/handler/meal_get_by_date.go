126f54f17cf5acb624fbf91f69eeeaa8ab8666f7
Wes <pimpbot@ufl.edu>
2023-04-04 21:16:07 -0500

    modified query to remove idents and retrieve a week of recipes

--- recipeApp/server/httpd/handler/meal_get_by_date.go
+++ recipeApp/server/httpd/handler/meal_get_by_date.go
@@ -30,12 +30,12 @@ func MealGetByDate() gin.HandlerFunc {
 		date := c.Query("date")
 
 		begin, _ := time.Parse("2006-01-02", date)
-		end := begin.AddDate(0, 0, 7)
+		end := begin.AddDate(0, 0, 6)
 
 		beginString := begin.Format("2006-01-02")
 		endString := end.Format("2006-01-02")
 
-		initialize.Db.Table("meals").Select("meals.mid, meals.userid, meals.recipeid, meals.date, meals.mealtype, recipe.title, recipe.ingredients, recipe.instructions, recipe.image_name, users.email, recipe.image").
+		initialize.Db.Table("meals").Select("meals.date, meals.mealtype, recipe.title, recipe.ingredients, recipe.instructions, recipe.image_name, users.email, recipe.image").
 			Joins("JOIN recipe ON meals.recipeid = recipe.rid").
 			Joins("JOIN users ON meals.userid = users.id").
 			Where("meals.userid = ? AND meals.date BETWEEN ? AND ?", uid, beginString, endString).
